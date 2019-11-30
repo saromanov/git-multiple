e5b83a19efb74306f46d58c89f8ccc503121315d
Sergey <xxsmotur@gmail.com>
2019-12-01 01:32:10 +0500

    Add command for exec

--- git-multiple.sh
+++ git-multiple.sh
@@ -15,12 +15,36 @@ list()
 {
     ls -lA $git_multiple_path | awk -F':[0-9]* ' '/:/{print $2}'
 }
+
+add() 
+{
+    filename=$git_multiple_path/$1
+    if [ -f "$filename" ]; then
+        echo $2 >> $filename
+    else
+        echo "File does not exist"
+    fi
+}
+
+execute() 
+{
+    filename=$git_multiple_path/$1
+    while read line; do
+        cd $line
+        $3
+        cd -
+    done < $filename
+}
 if [[ $1 == "init" ]]; then
 init
 elif [[ $1 == "new" ]]; then
 new $2
 elif [[ $1 == "ls" ]]; then
 list
+elif [[ $1 == "add" ]]; then 
+add $2 $3
+elif [[ $1 == "exec" ]]; then 
+execute $2 $3
 else
 echo "invalid command"
 fi

1a56f9221ab9f5984c43de10109343370ef9279b
Sergey <xxsmotur@gmail.com>
2019-12-01 00:40:46 +0500

    Add new and ls commands

--- git-multiple.sh
+++ git-multiple.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+git_multiple_path=$HOME/gitmultiple
 init()
 {
-    mkdir $HOME/gitmultiple
+    mkdir $git_multiple_path
+}
+
+new() 
+{
+    touch $git_multiple_path/$1
+}
+
+list() 
+{
+    ls -lA $git_multiple_path | awk -F':[0-9]* ' '/:/{print $2}'
 }
 if [[ $1 == "init" ]]; then
 init
+elif [[ $1 == "new" ]]; then
+new $2
+elif [[ $1 == "ls" ]]; then
+list
 else
 echo "invalid command"
 fi
